5385f167412db6235b2c65e68c97d874fd87e815
joelsitovs <alanjoelt43@gmail.com>
2025-06-17 05:10:33 +0200

    Update API URL to production endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
     appId: '1:750705921113:web:776493a20c3f7c990a7548',
     measurementId: 'G-4K15MWTGRT',
   },
-  stipeKey:'pk_test_51QRc80JWHbvf2wryew1VpDCWtsneGa7W0zzrdZaVnEJsQrve7dQUPlYhw64e8hP8lNSPgTwnTID0Rwfm3TYR9W8000E336CQjY',
-  serverUlr: 'https://api.morfeo3d.es:4242'
+  stipeKey:
+    'pk_test_51QRc80JWHbvf2wryew1VpDCWtsneGa7W0zzrdZaVnEJsQrve7dQUPlYhw64e8hP8lNSPgTwnTID0Rwfm3TYR9W8000E336CQjY',
+  serverUlr: 'https://api.morfeo3d.es/stripe',
 };
